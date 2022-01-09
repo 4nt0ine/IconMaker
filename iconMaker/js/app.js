648f1839e222ced199ecb5c3fd39a4d625d51ae9
4nt0ine <antoinelemeteil405@gmail.com>
2022-01-09 21:05:45 +0100

    new commit

--- iconMaker/js/app.js
+++ iconMaker/js/app.js
@@ -1,55 +1,85 @@
 // -----------------------------------------------------------
-$(function () {
-  function createGrid() {
-    let appScreen = $(".app-wrapper .app-screen");
-    console.log(appScreen);
-
-    all = 32 * 32;
-    for (let i = 0; i < all; i++) {
-      appScreen.append("<div class='pixel'></div>");
-    }
+//create de la grille 32*32 en ajoutant une div a chaque pixel
+function createGrid() {
+  let appScreen = $(".app-screen");
+  all = 32 * 32;
+  for (let i = 0; i < all; i++) {
+    appScreen.append("<div class='pixel'></div>");
   }
-  createGrid();
+}
 
-  //recuperer les couleurs et les sauvegarder
+function makeColors() {
+  // recuperer les couleurs
+  let bg_color = "";
+  //---------------------------
 
-  function makeColors() {
-    $("#paint_darkestgreen").click(function () {
-      color = "bg_darkestgreen";
-    });
-    $("#paint_darkgreen").click(function () {
-      color = "bg_darkgreen";
-    });
-    $("#paint_green").click(function () {
-      color = "bg_green";
-    });
-    $("#paint_lightgreen").click(function () {
-      color = "bg_lightgreen";
-    });
+  $("#paint_red").click(function () {
+    $("#paint_red").addClass("is-active");
+    $("#paint_darkgreen").removeClass("is-active");
+    $("#paint_brown").removeClass("is-active");
+    $("#paint_darkyellow").removeClass("is-active");
 
-    let pixel = $(".pixel");
-    let darkestgreen = $("#paint_darkestgreen").css("background-color");
-    console.log(darkestgreen);
-    pixel.css("color", darkestgreen);
+    bg_color = "bg_red";
+  });
+  $(".pixel").click(function () {
+    $(this).addClass(bg_color);
+  });
 
-    let darkgreen = $("#paint_darkgreen").css("background-color");
-    console.log(darkgreen);
-    pixel.css("color", darkgreen);
+  //-------------------------
+  //---------------------------
 
-    let green = $("#paint_green").css("background-color");
-    console.log(green);
-    pixel.css("color", green);
+  $("#paint_darkgreen").click(function () {
+    $("#paint_red").removeClass("is-active");
+    $("#paint_darkgreen").addClass("is-active");
+    $("#paint_brown").removeClass("is-active");
+    $("#paint_darkyellow").removeClass("is-active");
 
-    let lightgreen = $("#paint_lightgreen").css("background-color");
-    console.log(lightgreen);
-    pixel.css("color", lightgreen);
-  }
-  makeColors();
+    bg_color = "bg_darkgreen";
+  });
+  $(".pixel").click(function () {
+    $(this).addClass(bg_color);
+  });
 
-  //cliquer sur un bouton de couleur pour avoir la couleur
-});
+  //---------------------------
+
+  $("#paint_brown").click(function () {
+    $("#paint_red").removeClass("is-active");
+    $("#paint_darkgreen").removeClass("is-active");
+    $("#paint_brown").addClass("is-active");
+    $("#paint_darkyellow").removeClass("is-active");
+
+    bg_color = "bg_brown";
+  });
+  $(".pixel").click(function () {
+    $(this).addClass(bg_color);
+  });
+
+  //---------------------------
+
+  $("#paint_darkyellow").click(function () {
+    $("#paint_red").removeClass("is-active");
+    $("#paint_darkgreen").removeClass("is-active");
+    $("#paint_brown").removeClass("is-active");
+    $("#paint_darkyellow").addClass("is-active");
+
+    bg_color = "bg_darkyellow";
+  });
+  $(".pixel").click(function () {
+    $(this).addClass(bg_color);
+  });
+}
+
+// function doReset (){
+//     $("button #reset").click(function() {
+//         $("#content .pixel").reset();
+
+//     })
+// }
 
 $(document).ready(function () {
-  console.log("ready !");
+  createGrid();
+  makeColors();
+  // doReset();
 });
+
 // -----------------------------------------------------------
